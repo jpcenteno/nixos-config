1df6b74765e08eab3584db40f2fc8d9802a05718
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-02-22 22:00:23 -0300

    Move Gnome config to a nixos module

--- hosts/abend/configuration.nix
+++ hosts/abend/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/nixos/keyd/keyd.nix
+      ../../modules/nixos/desktop.nix
     ];
 
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
