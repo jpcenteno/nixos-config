e478f337e8157bffd82d1b30a2ffba6966925311
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-03-01 13:34:08 -0300

    Install JetBrains Mono

--- hosts/abend/home.nix
+++ hosts/abend/home.nix
@@ -33,11 +33,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
