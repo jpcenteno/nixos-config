2f6502d13c0431d43b18c188b2f3db5868a4d22f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-03-07 18:16:11 -0300

    Add docstring

--- modules/home-manager/librewolf.nix
+++ modules/home-manager/librewolf.nix
@@ -1,3 +1,13 @@
+# > A custom version of Firefox, focused on privacy, security and freedom.
+# >
+# > LibreWolf is designed to increase protection against tracking and
+# > fingerprinting techniques, while also including a few security improvements.
+# > This is achieved through our privacy and security oriented settings and
+# > patches. LibreWolf also aims to remove all the telemetry, data collection
+# > and annoyances, as well as disabling anti-freedom features like DRM.
+# >
+# > From https://librewolf.net/
+
 { ... }:
 {
   programs.librewolf = {
