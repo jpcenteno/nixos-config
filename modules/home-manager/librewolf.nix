394bae759930583fff361735e92ea5d56aecb2a2
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-03-13 20:13:12 -0300

    Set brave as default XDG opener for HTML/HTTPS/XML

--- modules/home-manager/librewolf.nix
+++ modules/home-manager/librewolf.nix
@@ -20,15 +20,4 @@
       "browser.translations.enable" = false;
     };
   };
-
-  # Taken from [1].
-  # [1]:  https://github.com/ngkz/dotfiles/blob/941fcbc7f30dab2254e744e187476648423ca922/home/workstation/librewolf/default.nix#L17
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = "librewolf.desktop";
-    "text/xml" = "librewolf.desktop";
-    "application/xhtml+xml" = "librewolf.desktop";
-    "application/vnd.mozilla.xul+xml" = "librewolf.desktop";
-    "x-scheme-handler/http" = "librewolf.desktop";
-    "x-scheme-handler/https" = "librewolf.desktop";
-  };
 }
