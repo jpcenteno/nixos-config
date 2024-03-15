d22d32dd2b5ee708dbc113d56b6ad9a814c700e6
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-03-15 19:13:22 -0300

    Enable Zathura document reader

---
+++ modules/home-manager/zathura.nix
@@ -0,0 +1,16 @@
+{ ... }:
+let
+  desktopEntry = "org.pwmt.zathura.desktop";
+in
+{
+  programs.zathura.enable = true;
+
+  xdg.mimeApps = {
+    defaultApplications = {
+      "application/pdf" = [desktopEntry];
+      "application/epub+zip" = [desktopEntry];
+      "application/vnd.comicbook+zip" = [desktopEntry];  # .cbz
+      "application/vnd.comicbook-rar" = [desktopEntry];  # .cbr
+    };
+  };
+}
