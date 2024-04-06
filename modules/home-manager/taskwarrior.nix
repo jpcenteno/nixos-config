9b3ea2ceecb4a87aa0ec89cb72d4d40751b5c481
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-04-06 17:34:44 -0300

    Set INBOX as the default project to capture new tasks

--- modules/home-manager/taskwarrior.nix
+++ modules/home-manager/taskwarrior.nix
@@ -3,6 +3,8 @@
   programs.taskwarrior = {
     enable = true;
     colorTheme = "dark-16";
+    # Set INBOX as the default project to capture new tasks.
+    config.default.project = "INBOX";
   };
 
   programs.bash.shellAliases.t = "task";
