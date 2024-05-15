dab1d6f7024dba025546f30a0a489688bd38b1bf
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-05-15 04:17:47 -0300

    Tmux: Base16 theme

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,4 +1,14 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.tmux ];
-  xdg.configFile."tmux/tmux.conf".source = ../../dotfiles/tmux/tmux.conf;
+{ config, ... }: {
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      source-file ${config.xdg.configHome}/tmux/general.conf
+      source-file ${config.xdg.configHome}/tmux/base16.conf
+    '';
+  };
+
+  xdg.configFile = {
+    "tmux/general.conf".source = ../../dotfiles/tmux/general.conf;
+    "tmux/base16.conf".source = ../../dotfiles/tmux/base16.conf;
+  };
 }
