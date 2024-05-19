ef68e52bc205f9ef3019ddf1f2761741b74349be
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-05-19 01:00:03 -0300

    Add python3 provider to nvim

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -4,5 +4,12 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true; # Sets $EDITOR.
+    viAlias = true;
+    vimAlias = true;
+    withPython3 = true;
+    extraPackages = [
+      # Test this by running `:checkhealth provider` in vim.
+      (pkgs.python3.withPackages (python-packages: [ python-packages.pynvim ]))
+    ];
   };
 }
