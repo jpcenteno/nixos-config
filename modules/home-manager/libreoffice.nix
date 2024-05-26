bc27b83419fb9e7cd667cbcbee9d04a0b95ae578
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-05-26 20:57:22 -0300

    Refactor LibreOffice module
    This commit reshapes the `libreoffice.nix` module in a way that sets the
    precedent for other modules that require a behavior when set to
    disabled.

--- modules/home-manager/libreoffice.nix
+++ modules/home-manager/libreoffice.nix
@@ -3,19 +3,17 @@ let cfg = config.self.libreoffice;
 in {
   options.self.libreoffice = { enable = lib.mkEnableOption "libreoffice"; };
 
-  config = {
-    # When enabled:
-    home.packages = lib.mkIf cfg.enable [ pkgs.libreoffice ];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable { home.packages = [ pkgs.libreoffice ]; })
 
-    # When disabled:
-    home.activation = lib.mkIf (cfg.enable == false) {
+    (lib.mkIf (!cfg.enable) {
       # Infuriatingly, LibreOffice uses `$XDG_CONFIG_DIR` to store caches. This
       # ensures that it's configuration directory gets deleted when disabled to
       # prevent oblivious storage of personal data.
-      cleanup-libreoffice-dotfiles =
+      home.activation.cleanup-libreoffice-dotfiles =
         lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           $DRY_RUN_CMD rm $VERBOSE_ARG -rf "${config.xdg.configHome}/libreoffice"
         '';
-    };
-  };
+    })
+  ];
 }
