215778cf2f296710673f03e8c3b3b24b940e0598
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-23 20:59:18 -0300

    Use viu to preview images

--- modules/home-manager/lf.nix
+++ modules/home-manager/lf.nix
@@ -13,14 +13,19 @@
         text = ''
           #!/bin/sh
 
-          case "$1" in
-              *.tar*) "${pkgs.gnutar}/bin/tar" tf "$1";;
-              *.zip) "${pkgs.unzip}/bin/unzip" -l "$1";;
-              *.rar) "${pkgs.unrar}/bin/unrar" l "$1";;
-              # *.7z) 7z l "$1";;
-              # *.pdf) pdftotext "$1" -;;
-              *) "${pkgs.bat}/bin/bat" "$1";;
+          mime="$( "${pkgs.file}/bin/file" --dereference --brief --mime-type -- "$1" )"
+          case "$mime" in
+            image/*) "${pkgs.viu}/bin/viu" "$1" ;;
           esac
+
+          # case "$1" in
+          #     *.tar*) "${pkgs.gnutar}/bin/tar" tf "$1";;
+          #     *.zip) "${pkgs.unzip}/bin/unzip" -l "$1";;
+          #     *.rar) "${pkgs.unrar}/bin/unrar" l "$1";;
+          #     # *.7z) 7z l "$1";;
+          #     # *.pdf) pdftotext "$1" -;;
+          #     *) "${pkgs.bat}/bin/bat" "$1";;
+          # esac
         '';
       };
     };
