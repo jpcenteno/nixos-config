25b0473df7aa4897e7625db8cc3783b20cd3b437
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-23 21:08:18 -0300

    LF preview: use MIME type to select preview tool.
    I know that the man page recommends against it, but it's easier to
    implement than listing all the image types that `viu` might support.

--- modules/home-manager/lf.nix
+++ modules/home-manager/lf.nix
@@ -15,17 +15,12 @@
 
           mime="$( "${pkgs.file}/bin/file" --dereference --brief --mime-type -- "$1" )"
           case "$mime" in
-            image/*) "${pkgs.viu}/bin/viu" "$1" ;;
+            image/*)           "${pkgs.viu}/bin/viu" "$1"        ;;
+            application/zip)   "${pkgs.unzip}/bin/unzip" -l "$1" ;;
+            application/x-rar) "${pkgs.unrar}/bin/unrar" l "$1"  ;;
+            application/x-tar) "${pkgs.gnutar}/bin/tar" -tf "$1" ;;
+            *)                 "${pkgs.bat}/bin/bat" "$1"        ;;
           esac
-
-          # case "$1" in
-          #     *.tar*) "${pkgs.gnutar}/bin/tar" tf "$1";;
-          #     *.zip) "${pkgs.unzip}/bin/unzip" -l "$1";;
-          #     *.rar) "${pkgs.unrar}/bin/unrar" l "$1";;
-          #     # *.7z) 7z l "$1";;
-          #     # *.pdf) pdftotext "$1" -;;
-          #     *) "${pkgs.bat}/bin/bat" "$1";;
-          # esac
         '';
       };
     };
