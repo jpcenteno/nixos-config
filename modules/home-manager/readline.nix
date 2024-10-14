aa71c605ab781982e1e7ea27255b8bf439674eee
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-10-14 14:12:23 -0300

    Comment out readline config to test that it does not work anymore

--- modules/home-manager/readline.nix
+++ modules/home-manager/readline.nix
@@ -7,9 +7,9 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.readline = {
-      enable = true;
-      extraConfig = builtins.readFile ../../dotfiles/readline/inputrc;
-    };
+    # programs.readline = {
+    #   enable = true;
+    #   extraConfig = builtins.readFile ../../dotfiles/readline/inputrc;
+    # };
   };
 }
