c8257ad4af1619a4126e1ced6bf2e2befaa1c142
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-10-14 01:37:31 -0300

    Install bat using packages list instead of home-manager options.
    This is better for cases where only `programs.<program>.enable = true`
    is used because home-manager options are more likely (IMHO) than package
    names.

--- modules/home-manager/shell/bat.nix
+++ modules/home-manager/shell/bat.nix
@@ -1,11 +1,11 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let cfg = config.jpcenteno-home.shell.extras.bat;
 in {
 
   options.jpcenteno-home.shell.extras.bat = { enable = lib.mkEnableOption "Bat"; };
 
   config = lib.mkIf cfg.enable {
-    programs.bat.enable = true;
+    home.packages = [ pkgs.bat ];
     xdg.configFile."bat/config".source = ../../../dotfiles/bat/config;
   };
 }
