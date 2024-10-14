96d847e747882718946183268df55acb0e18a360
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-10-14 01:42:10 -0300

    Add a note to self about `alias cat=bat`
    Added a note-to-self at the `bat.nix` file in case my future self has to
    debug some issue related to where the `alias cat=bat` comes from.

--- modules/home-manager/shell/bat.nix
+++ modules/home-manager/shell/bat.nix
@@ -8,4 +8,8 @@ in {
     home.packages = [ pkgs.bat ];
     xdg.configFile."bat/config".source = ../../../dotfiles/bat/config;
   };
+
+  # NOTE 2024-10-14:
+  # Dotfile `../../../dotfiles/bash/bashrc` contains a conditional alias that
+  # overrides `cat` with `bat` on interactive shells in case bat is available.
 }
