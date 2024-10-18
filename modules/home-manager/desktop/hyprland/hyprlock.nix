3b8e27741fefbe7573846c33244f5f94d916604b
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-10-18 14:15:35 -0300

    Hyprlock: Update theme

--- modules/home-manager/desktop/hyprland/hyprlock.nix
+++ modules/home-manager/desktop/hyprland/hyprlock.nix
@@ -1,42 +1,60 @@
 { config, lib, ... }:
 let
   cfg = config.jpcenteno-home.desktop.hyprland.hyprlock;
+
+  colors = with config.colorScheme.palette; {
+    background = base00;
+    backgroundLighter = base01;
+    foreground = base05;
+    fail = base08;
+    checking = base0A;
+  };
 in {
   options.jpcenteno-home.desktop.hyprland.hyprlock = {
     enable = lib.mkEnableOption "Hyprlock";
   };
 
   config = lib.mkIf cfg.enable {
-    programs.hyprlock = let
-      palette = config.colorScheme.palette; 
-    in {
+    programs.hyprlock = {
       enable = true;
       settings = {
         background = {
-          monitor = "";
-          color = "rgb(${palette.base00})";
+          monitor = ""; # Apply to every monitor.
+          color = "rgb(${colors.background})";
 
           blur_passes = 0; # Deactivate.
         };
 
         input-field = {
-          monitor = "";
+          # Positioning:
+          monitor = ""; # Apply to every monitor.
           size = "200,50";
+          position = "0, -100";
+          halign = "center";
+          valign = "center";
+
+          fade_on_empty = false;
+
+          # Prompt:
+          inner_color = "rgb(${colors.backgroundLighter})";
+          hide_input = false;
+          rounding = 0;
+
+          # Prompt (Waiting for password):
           outline_thickness = 2;
           dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
           dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
           dots_center = true;
-          outer_color = "rgb(${palette.base05})";
-          inner_color = "rgb(${palette.base01})";
-          font_color = "rgb(${palette.base05})";
-          fade_on_empty = false;
-          rounding = -1;
-          check_color = "rgb(30, 107, 204)";
-          placeholder_text = ''<span foreground="##${palette.base05}">Password</span>'';
-          hide_input = false;
-          position = "0, -100";
-          halign = "center";
-          valign = "center";
+          outer_color = "rgb(${colors.foreground})";
+          font_color = "rgb(${colors.foreground})";
+          placeholder_text = ''<span foreground="##${colors.foreground}">Password</span>'';
+
+          # Prompt (checking password):
+          check_color = "rgb(${colors.checking})";
+
+          # Prompt (Failure):
+          fail_color = "rgb(${colors.fail})"; # Changes outer_color and fail message color
+          fail_text = "$FAIL #$ATTEMPTS"; # `$FAIL` = PAM fail reason.
         };
       };
     };
