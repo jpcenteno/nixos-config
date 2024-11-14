ebace4a588df1a1ef2bd799369e9c2b0c5bb992c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-11-14 10:31:40 -0300

    Add smug.enable option to tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -5,10 +5,15 @@ in
 {
   options.jpcenteno-home.tmux = {
     enable = lib.mkEnableOption "Enable Tmux with my personal config";
+
+    smug.enable = lib.mkEnableOption "Smug";
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.tmux ];
+    home.packages = [
+      pkgs.tmux
+      (lib.mkIf cfg.smug.enable pkgs.smug)
+    ];
     xdg.configFile."tmux/tmux.conf".source = ../../dotfiles/tmux/tmux.conf;
   };
 }
