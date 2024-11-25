be3923290c94b3452acd6492ca2820b99989df5f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-11-25 03:45:59 +0000

    Set default colorscheme at the home-manager module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,10 +1,12 @@
-{ config, lib, ... }:
+{ config, lib, nix-colors, ... }:
 let
   cfg = config.jpcenteno-home;
 in {
   imports = [
     ./shell/default.nix
     ./xdg.nix
+
+    nix-colors.homeManagerModules.default
   ];
 
   options.jpcenteno-home = {
@@ -12,6 +14,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    # Set the default color scheme.
+    colorScheme = nix-colors.colorSchemes.rose-pine;
+
+    # These modules are enabled by default across all my home-manager configs:
     jpcenteno-home.shell.enable = lib.mkDefault true;
   };
 }
