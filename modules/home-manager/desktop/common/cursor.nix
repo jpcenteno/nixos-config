ea22426590ce530b4742d4ae0c9640d4a250abfa
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-12-01 17:25:21 +0000

    cursor: Create module that sets the cursor size and theme

---
+++ modules/home-manager/desktop/common/cursor.nix
@@ -0,0 +1,17 @@
+{ config, lib, ... }:
+let
+  cfg = config.jpcenteno-home.desktop.common.cursor;
+in {
+  options.jpcenteno-home.home.desktop.common.cursor = {
+    enable = lib.mkEnableOption "Cursor theme and size customization";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.apple-cursor;
+      name = "macOS-BigSur";
+      size = 32;
+    };
+  };
+}
