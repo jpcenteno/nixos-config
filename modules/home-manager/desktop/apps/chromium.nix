8586f7d8abbc4095b6a14d9a71938bece66ba33c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-12-19 17:57:35 -0300

    Allow Ungoogled Chromium to install extensions

--- modules/home-manager/desktop/apps/chromium.nix
+++ modules/home-manager/desktop/apps/chromium.nix
@@ -1,6 +1,37 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.jpcenteno-home.desktop.apps.chromium;
+
+  ungoogledChromiumFlags = [
+
+    # Prompt for install when downloading Chromium extensions (`.crx`
+    # files) or User Scripts.
+    #
+    # This is required for installing `NeverDecaf/chromium-web-store`.
+    #
+    # ## Relevant documentation:
+    #
+    # > Change how extension MIME types (CRX and user scripts) are
+    # > handled. Acceptable values are `download-as-regular-file` or
+    # > `always-prompt-for-install`. Leave unset to use normal behavior.
+    # >
+    # > -- https://github.com/ungoogled-software/ungoogled-chromium/blob/master/docs/flags.md
+    #
+    # > Handling of extension MIME type requests
+    # > Used when deciding how to handle a request for a CRX or User
+    # > Script MIME type. ungoogled-chromium flag. – Mac, Windows,
+    # > Linux, ChromeOS, Android, Lacros.
+    # >
+    # > -- chrome://flags/#extension-mime-request-handling
+    #
+    # ## How to Debug:
+    #
+    # 1. Relaunch `ungoogled-chromium`.
+    # 2. Check `chrome://flags/#extension-mime-request-handling`.
+    "--extension-mime-request-handling=always-prompt-for-install"
+
+  ];
+
 in
 {
   options.jpcenteno-home.desktop.apps.chromium = {
@@ -15,6 +46,9 @@ in
     package = lib.mkPackageOption pkgs "Chromium" {
       default = "brave";
     };
+
+    # FIXME autodetect ungoogled chromium or throw a warning, IDK.
+    enableUngoogledChromiumFlags = lib.mkEnableOption "Ungoogled-chromium specific flags";
   };
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
@@ -23,9 +57,13 @@ in
         enable = true;
         package = cfg.package;
         # FIXME this will break non-Wayland or non PipeWire hosts.
-        commandLineArgs = [
-          "--ozone-platform=wayland"
-          "--enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+        commandLineArgs = lib.concatLists [
+          [
+            "--ozone-platform=wayland"
+            "--enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+          ]
+
+          (lib.optionals cfg.enableUngoogledChromiumFlags ungoogledChromiumFlags)
         ];
       };
     }
