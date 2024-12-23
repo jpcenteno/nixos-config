7f69844d66c88218ebf63766ce533e46c08e45ae
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-12-23 14:38:15 -0300

    lf: refactor into a proper home-manager module

--- modules/home-manager/utils/lf.nix
+++ modules/home-manager/utils/lf.nix
@@ -1,28 +1,37 @@
-{ config, pkgs, ... }: {
-  home.packages = [ pkgs.lf ];
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.jpcenteno-home.utils.lf;
+in {
+  options.jpcenteno-home.utils.lf = {
+    enable = lib.mkEnableOption "LF - Terminal file manager";
+  };
 
-  xdg = {
-    enable = true;
-    configFile = {
-      "lf/lfrc".text = ''
-        set previewer ${config.xdg.configHome}/lf/previewer.sh
-      '';
+  config = lib.mkIf cfg.enable {
+    home.packages = [ pkgs.lf ];
 
-      "lf/previewer.sh" = {
-        executable = true;
-        text = ''
-          #!/bin/sh
-
-          mime="$( "${pkgs.file}/bin/file" --dereference --brief --mime-type -- "$1" )"
-          case "$mime" in
-            image/*)           "${pkgs.viu}/bin/viu" "$1"        ;;
-            application/zip)   "${pkgs.unzip}/bin/unzip" -l "$1" ;;
-            application/x-rar) "${pkgs.unrar}/bin/unrar" l "$1"  ;;
-            application/x-tar) "${pkgs.gnutar}/bin/tar" -tf "$1" ;;
-            application/json)  "${pkgs.jq}/bin/jq" --color-output . "$1" ;;
-            *)                 "${pkgs.bat}/bin/bat" --color always "$1" ;;
-          esac
+    xdg = {
+      enable = true;
+      configFile = {
+        "lf/lfrc".text = ''
+          set previewer ${config.xdg.configHome}/lf/previewer.sh
         '';
+
+        "lf/previewer.sh" = {
+          executable = true;
+          text = ''
+            #!/bin/sh
+
+            mime="$( "${pkgs.file}/bin/file" --dereference --brief --mime-type -- "$1" )"
+            case "$mime" in
+              image/*)           "${pkgs.viu}/bin/viu" "$1"        ;;
+              application/zip)   "${pkgs.unzip}/bin/unzip" -l "$1" ;;
+              application/x-rar) "${pkgs.unrar}/bin/unrar" l "$1"  ;;
+              application/x-tar) "${pkgs.gnutar}/bin/tar" -tf "$1" ;;
+              application/json)  "${pkgs.jq}/bin/jq" --color-output . "$1" ;;
+              *)                 "${pkgs.bat}/bin/bat" --color always "$1" ;;
+            esac
+          '';
+        };
       };
     };
   };
