11166ecbd72c6ec8ad272a4f1d7f259e501e4846
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-09 18:01:34 -0300

    Move starship config. Disable some annoying modules

---
+++ modules/home-manager/shell/extras/starship.nix
@@ -0,0 +1,30 @@
+{ config, lib, ... }:
+let
+  cfg = config.jpcenteno-home.shell.extras.starship;
+in {
+  options.jpcenteno-home.shell.extras.starship = {
+    enable = lib.mkEnableOption "Starship";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.starship = {
+      enable = true;
+
+      # Shell integrations.
+      enableBashIntegration = lib.mkDefault true;
+      enableFishIntegration = lib.mkDefault true;
+      enableZshIntegration = lib.mkDefault true;
+
+      settings = {
+
+        # Disable version info.
+        bun.disabled = lib.mkDefault true;
+        conda.disabled = lib.mkDefault true;
+        nodejs.disabled = lib.mkDefault true;
+      };
+    };
+
+
+    
+  };
+}
