dd4343160b38b8ab6d49ee23103d5ae1dbb21b5f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-10 22:13:16 -0300

    Create admin tools module hierarchy, install lsusb

---
+++ modules/nixos/admin/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.jpcenteno.nixos.admin;
+in {
+  options.jpcenteno.nixos.admin = {
+    enable = lib.mkEnableOption "Admin tools";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      usbutils # Provides lsusb usb-devices usbhid-dump usbreset.
+    ];
+  };
+}
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,12 +5,14 @@ in {
   imports = [
     ./nix/default.nix
     ./system/default.nix
+    ./admin/default.nix
   ];
   options.jpcenteno.nixos = {
     enable = lib.mkEnableOption "Jpcenteno's NixOS config";
   };
 
   config = lib.mkIf cfg.enable {
+    jpcenteno.nixos.admin.enable = lib.mkDefault true;
     jpcenteno.nixos.nix.enable = lib.mkDefault true;
     jpcenteno.nixos.system.enable = lib.mkDefault true;
   };
