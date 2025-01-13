749fc9bd1fc0236e2d3540e6ace7bf2b15bb65e9
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-13 12:41:10 -0300

    GitHub: Remove faulty (and redundant) assertion.
    This assertion was made redundant when I decided to move `github.nix`
    under the `git.nix` module hierarchy. Except if you run into a corner
    case where you disable the `git.nix` module and enable the `github`
    module, but that's a weird thing to.

--- modules/home-manager/development/github.nix
+++ modules/home-manager/development/github.nix
@@ -7,16 +7,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        # Using this to ensure that Git is installed and configured. It would make
-        # no sense to configure the Github CLI without Git itself.
-        # jpcenteno-home.git.enable = lib.mkDefault true;
-        assertion = (config.programs.git.enable == true);
-        message = "GitHub tools require `programs.git` to be enabled";
-      }
-    ];
-
     # GitHub CLI and extensions.
     programs.gh = {
       enable = lib.mkforce cfg.github-cli.enable;
