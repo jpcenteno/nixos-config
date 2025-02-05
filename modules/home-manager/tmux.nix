20ffe02bf61942d891e590517d25874b95483018
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-02-05 18:24:14 -0300

    feat: Remove unused smug

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -5,22 +5,10 @@ in
 {
   options.jpcenteno-home.tmux = {
     enable = lib.mkEnableOption "Enable Tmux with my personal config";
-
-    smug.enable = lib.mkEnableOption "Smug";
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      pkgs.tmux
-      (lib.mkIf cfg.smug.enable pkgs.smug)
-    ];
+    home.packages = [ pkgs.tmux ];
     xdg.configFile."tmux/tmux.conf".source = ../../dotfiles/tmux/tmux.conf;
-
-    # Ensure $XDG_CONFIG_HOME/smug exists.
-    home.activation.create-smug-config-dir = lib.mkIf cfg.smug.enable (
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD mkdir -p $VERBOSE_ARG '${config.xdg.configHome}/smug'
-      ''
-    );
   };
 }
