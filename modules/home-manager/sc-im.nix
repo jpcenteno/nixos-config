e7cedbf5dad945e0b0838d7a1e65bf372d25ef8e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-02-08 10:11:13 -0300

    chore(inputs): Remove nixpkgs-unstable flake input

--- modules/home-manager/sc-im.nix
+++ modules/home-manager/sc-im.nix
@@ -11,13 +11,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    # FIXME Update the system to NixOs 24.05, then switch to stable packages.
-    #
-    # SC-IM has an optional dependency on `libxls`, which is marked as insecure.
-    # Neither I care for this feature.
-    #
-    # Im still on v23.11, but starting from 24.05, `sc-im` Xlsx support was made
-    # optional and disabled by default.
-    home.packages = [pkgs-unstable.sc-im];
+    home.packages = [pkgs.sc-im];
   };
 }
