8cbbdffaaf9521c5816769427e5c87d71b9e84d8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-02-09 11:43:14 -0300

    chore(flake): Switch to the unstable nixpkgs branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {nixpkgs, ...}: let
