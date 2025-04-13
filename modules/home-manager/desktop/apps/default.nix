bbe5b76c61ae48775bcc8b2e48e0d70c3d0b900d
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-04-12 22:46:09 -0300

    feat: Add FreeCAD to desktop apps module

--- modules/home-manager/desktop/apps/default.nix
+++ modules/home-manager/desktop/apps/default.nix
@@ -24,6 +24,7 @@ in {
     mpv.enable = lib.mkEnableOption "Mpv Video Player" // {default = true;};
     gnumeric.enable = lib.mkEnableOption "Gnumeric" // {default = true;};
     libreoffice.enable = lib.mkEnableOption "Libreoffice" // {default = true;};
+    freecad.enable = lib.mkEnableOption "freecad" // {default = true;};
   };
 
   config = lib.mkIf cfg.enable {
@@ -44,6 +45,7 @@ in {
       (lib.mkIf cfg.mpv.enable pkgs.mpv)
       (lib.mkIf cfg.gnumeric.enable pkgs.gnumeric)
       (lib.mkIf cfg.libreoffice.enable pkgs.libreoffice)
+      (lib.mkIf cfg.freecad.enable pkgs.freecad-wayland)
     ];
 
     # FIXME 2024-12-07 Uncomment once I fix the issue with the activation script
