5db83fc7159ace99a831c96338b443a343c4172a
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-08-04 18:29:53 -0300

    ollama: Make the systemd service optional

--- modules/home-manager/ai/ollama.nix
+++ modules/home-manager/ai/ollama.nix
@@ -5,15 +5,30 @@ in {
   options.jpcenteno-home.ai.ollama = {
     enable = lib.mkEnableOption "Ollama";
     package = lib.mkPackageOption pkgs "ollama" {};
-    # FIXME add a shortcut to toggle between the cuda and non-cuda version of
+    service.enable = lib.mkEnableOption "Ollama server service" // {
+      # FIXME (Mac OS support) Set this to `true` after setting up a service
+      # using `launchd`.
+      default = pkgs.stdenv.isLinux;
+      example = false;
+    };
+
+    # FIXME add a shortcut to toggle between the CUDA and non-CUDA version of
     # the Ollama package.
-    # FIXME Add an option to disable the ollama service. 
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        # FIXME (Mac OS support) Remove this assertion before implementing
+        # an equivalent service for Mac OS using `launchd`.
+        assertion = !cfg.service.enable || pkgs.stdenv.isLinux;
+        message = "Ollama service is only supported on Linux.";
+      }
+    ];
+
     home.packages = [ cfg.package ];
 
-    systemd.user.services.ollama = {
+    systemd.user.services.ollama = lib.mkIf (pkgs.stdenv.isLinux && cfg.service.enable) {
       Unit = {
         Description = "Ollama server";
         After = [ "network.target" ];
