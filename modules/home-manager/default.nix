2967aa22e9acbd38314d647578ff68acf97257a2
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-08-10 19:29:48 -0300

    colorscheme: Use everforest

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -24,7 +24,7 @@ in
 
   config = lib.mkIf cfg.enable {
     # Set the default color scheme.
-    colorScheme = nix-colors.colorSchemes.catppuccin-macchiato;
+    colorScheme = nix-colors.colorSchemes.everforest;
 
     # These modules are enabled by default across all my home-manager configs:
     jpcenteno-home = {
