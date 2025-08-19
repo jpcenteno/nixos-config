2a052492c2555f73cc2fad69c8234861c2199d57
Joaquín Centeno <jpcenteno@users.noreply.github.com>
2025-08-19 04:29:54 -0300

    Setup delta for diffing in git

---
+++ modules/home-manager/development/default.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+let
+  cfg = config.jpcenteno-home.development;
+in
+{
+  imports = [
+    ./delta.nix
+    # FIXME include these:
+    # ./github.nix
+    # ./nix-lsp.nix
+  ];
+
+  options.jpcenteno-home.development = {
+    enable = lib.mkEnableOption "development modules";
+  };
+
+  config = lib.mkIf cfg.enable {
+    jpcenteno-home.development = {
+      delta.enable = lib.mkDefault true;
+    };
+  };
+}
