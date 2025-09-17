95c8689f7dde87a9702812462a9f4e18da782619
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-09-17 19:01:53 -0300

    change colorscheme to rose-pine-moon

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -26,7 +26,7 @@ in
 
   config = lib.mkIf cfg.enable {
     # Set the default color scheme.
-    colorScheme = nix-colors.colorSchemes.everforest;
+    colorScheme = nix-colors.colorSchemes.rose-pine-moon;
 
     # These modules are enabled by default across all my home-manager configs:
     jpcenteno-home = {
