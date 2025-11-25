0e2f608831f5db707c40ce10f72ba0e0d46a8e87
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-11-25 10:10:39 -0300

    refactor(neovim): Make the module toggleable

--- modules/home-manager/development/neovim.nix
+++ modules/home-manager/development/neovim.nix
@@ -1,14 +1,30 @@
-{ pkgs, ... }:
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true; # Sets $EDITOR.
-    viAlias = true;
-    vimAlias = true;
-    withPython3 = true;
-    extraPackages = [
-      # Test this by running `:checkhealth provider` in vim.
-      (pkgs.python3.withPackages (python-packages: [ python-packages.pynvim ]))
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.jpcenteno-home.development.neovim;
+in
+{
+  options.jpcenteno-home.development.neovim = {
+    enable = lib.mkEnableOption "Neovim";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true; # Sets $EDITOR.
+      viAlias = true;
+      vimAlias = true;
+      withPython3 = true;
+      plugins = with pkgs.vimPlugins; [
+      ];
+      extraPackages = with pkgs; [
+        # Test this by running `:checkhealth provider` in vim.
+        (python3.withPackages (python-packages: [ python-packages.pynvim ]))
+      ];
+    };
   };
 }
