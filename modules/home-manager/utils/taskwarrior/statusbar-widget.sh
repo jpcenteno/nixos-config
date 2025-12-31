fb20b83566316bd68a6e3abf58aeab956b5d28ce
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-12-31 16:44:47 -0300

    refactor(taskwarrior): Make statusbar widget read the config only once

--- modules/home-manager/utils/taskwarrior/statusbar-widget.sh
+++ modules/home-manager/utils/taskwarrior/statusbar-widget.sh
@@ -1,8 +1,26 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
+TASKWARRIOR_CONFIG="" # Used to cache the taskwarrior config.
+
+_taskwarrior_config() {
+    if [ -z "$TASKWARRIOR_CONFIG"  ]; then
+        TASKWARRIOR_CONFIG="$( task _show )"
+        readonly TASKWARRIOR_CONFIG
+    fi
+
+    echo "$TASKWARRIOR_CONFIG"
+}
+
+_taskwarrior_config_get() {
+    local key="$1"
+    local sep='='
+    _taskwarrior_config | K="$key" awk -F "$sep" '$1 == ENVIRON["K"]' | cut -d "$sep" -f 2-
+}
+
 _get_filter_by_report_name() {
-    task _show | grep "^report.$1.filter=" | cut -d '=' -f '2-'
+    local name="$1"
+    _taskwarrior_config_get "report.$name.filter"
 }
 
 _count_tasks_for_report() {
