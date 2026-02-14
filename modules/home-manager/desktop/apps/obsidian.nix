a8cd41a0fe005715e972a3a9a737f4605a14e7e1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-02-14 20:21:37 -0300

    fix(Obsidian): Fix Obsidian web clipper
    The Obsidian Web Clipper extension was unable to save any of the
    clipping contents because _Focus on activate_ was disabled.
    
    This commit modifies the Hyprland config from the Obsidian module. This
    is generally ok for app-specific changes. In this case, the new setting
    affects Hyprland globally. This should be refactored to a more specific
    fix in the future.

--- modules/home-manager/desktop/apps/obsidian.nix
+++ modules/home-manager/desktop/apps/obsidian.nix
@@ -8,5 +8,18 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.obsidian ];
+
+    # This solves a problem with the Obsidian Web Clipper where only the title
+    # of the note was being saved, but neither the content, nor the frontmatter
+    # were being written to it.
+    #
+    # Note that this is a Hyprland-specific solution and this setting will only
+    # take effect when `wayland.windowManager.hyprland.enable` is set to `true`.
+    #
+    # https://help.obsidian.md/web-clipper/troubleshoot#Obsidian+opens+but+only+the+file+name+is+saved
+    #
+    # FIXME Replace this with a rule that only applies to Obsidian, so that this
+    # module does not affect Hyprland preferences in general.
+    wayland.windowManager.hyprland.settings.misc.focus_on_activate = true;
   };
 }
