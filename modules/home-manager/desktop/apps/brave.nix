c825827f04382c096979ed2687b5e268915aa29a
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-02-16 04:01:33 -0300

    feat(Brave): Install Brave and extensions

---
+++ modules/home-manager/desktop/apps/brave.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.jpcenteno-home.desktop.apps.brave;
+in {
+  options.jpcenteno-home.desktop.apps.brave = {
+    enable = lib.mkEnableOption "Brave browser";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.brave = {
+      enable = true;
+      package = lib.mkForce pkgs.brave;
+      extensions = [
+        { id = "jinjaccalgkegednnccohejagnlnfdag"; } # ViolentMonkey
+        { id = "pncfbmialoiaghdehhbnbhkkgmjanfhe"; } # uBlacklist
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
+        { id = "chphlpgkkbolifaimnlloiipkdnihall"; } # OneTab
+      ];
+    };
+  };
+}
