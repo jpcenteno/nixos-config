ade01ff9b7560753f520a800fe4ce01bac57632f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-02-17 04:14:55 -0300

    feat(keepasxc, brave): Install keepassxc browser extension

--- modules/home-manager/desktop/apps/keepasxc.nix
+++ modules/home-manager/desktop/apps/keepasxc.nix
@@ -10,9 +10,13 @@ in
 {
   options.jpcenteno-home.desktop.apps.keepassxc = {
     enable = lib.mkEnableOption "KeepassXC";
+
+    enableChromiumIntegration = lib.mkEnableOption "Chromium extension" // { default = true; };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.keepassxc ];
+
+    programs.chromium.extensions = lib.optional cfg.enableChromiumIntegration { id = "oboonakemofpalcgghocfoadofidjkkk"; };
   };
 }
