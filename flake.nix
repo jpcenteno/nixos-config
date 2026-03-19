f9b4059818013adc145f7f1f7ab85d1bc383936e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-03-19 16:22:13 -0300

    refactor(flake): Extract flake checks module

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
+        ./modules/flake/checks.nix
         ./modules/flake/formatting.nix
       ];
 
@@ -28,42 +29,6 @@
             packages = with pkgs; [ nil ];
           };
 
-          checks= {
-            nil = pkgs.stdenv.mkDerivation {
-              name = "linter";
-              src = ./.;
-              dontBuild = true;
-              doCheck = true;
-              nativeBuildInputs = [ pkgs.nil ];
-              checkPhase = ''
-                failed=""
-                find . -name "*.nix" | while read -r file; do
-                  echo "Linting: $file"
-
-                  diagnostics="$(nil diagnostics "$file")"
-                  if [ -n "$diagnostics" ]; then
-                    echo "$diagnostics"
-                    failed="yes"
-                  fi
-                done
-
-                test "$failed" != yes
-              '';
-              installPhase = "mkdir $out"; # Will fail otherwise.
-            };
-
-            statix = pkgs.stdenv.mkDerivation {
-              name = "statix";
-              src = ./.;
-              dontBuild = true;
-              doCheck = true;
-              nativeBuildInputs = [ pkgs.statix ];
-              checkPhase = ''
-                statix check .
-              '';
-              installPhase = "mkdir $out"; # Will fail otherwise.
-            };
-          };
       };
 
       flake = {
