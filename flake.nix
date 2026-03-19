2b5626419f78a4871b3e32ac90521150408c3cd8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-03-19 08:14:18 -0300

    refactor(flake): Wrap code inside a mkFlake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,11 @@
   description = "Nixos config flake";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -11,75 +15,80 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      systems,
-      treefmt-nix,
-      ...
-    }:
-    let
-      # Small tool to iterate over each systems
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      flake =
+        let
+          inherit (inputs)
+            self
+            nixpkgs
+            systems
+            treefmt-nix
+            ;
+          # Small tool to iterate over each systems
+          eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
 
-      # Eval the treefmt modules from ./treefmt.nix
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-    in
-    {
-      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+          system = "x86_64-linux";
+          pkgs = nixpkgs.legacyPackages.${system};
 
-      checks = eachSystem (pkgs: {
-        format = treefmtEval.${pkgs.system}.config.build.check self;
+          # Eval the treefmt modules from ./treefmt.nix
+          treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+        in
+        {
+          formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
-        nil = pkgs.stdenv.mkDerivation {
-          name = "linter";
-          src = ./.;
-          dontBuild = true;
-          doCheck = true;
-          nativeBuildInputs = [ pkgs.nil ];
-          checkPhase = ''
-            failed=""
-            find . -name "*.nix" | while read -r file; do
-              echo "Linting: $file"
+          checks = eachSystem (pkgs: {
+            format = treefmtEval.${pkgs.system}.config.build.check self;
 
-              diagnostics="$(nil diagnostics "$file")"
-              if [ -n "$diagnostics" ]; then
-                echo "$diagnostics"
-                failed="yes"
-              fi
-            done
+            nil = pkgs.stdenv.mkDerivation {
+              name = "linter";
+              src = ./.;
+              dontBuild = true;
+              doCheck = true;
+              nativeBuildInputs = [ pkgs.nil ];
+              checkPhase = ''
+                failed=""
+                find . -name "*.nix" | while read -r file; do
+                  echo "Linting: $file"
 
-            test "$failed" != yes
-          '';
-          installPhase = "mkdir $out"; # Will fail otherwise.
+                  diagnostics="$(nil diagnostics "$file")"
+                  if [ -n "$diagnostics" ]; then
+                    echo "$diagnostics"
+                    failed="yes"
+                  fi
+                done
+
+                test "$failed" != yes
+              '';
+              installPhase = "mkdir $out"; # Will fail otherwise.
+            };
+
+            statix = pkgs.stdenv.mkDerivation {
+              name = "statix";
+              src = ./.;
+              dontBuild = true;
+              doCheck = true;
+              nativeBuildInputs = [ pkgs.statix ];
+              checkPhase = ''
+                statix check .
+              '';
+              installPhase = "mkdir $out"; # Will fail otherwise.
+            };
+          });
+
+          devShells.${system}.default = pkgs.mkShell {
+            packages = with pkgs; [ nil ];
+          };
+
+          nixosModules = {
+            default = import ./modules/nixos/default.nix;
+          };
+
+          homeManagerModules = {
+            default = import ./modules/home-manager;
+          };
         };
-
-        statix = pkgs.stdenv.mkDerivation {
-          name = "statix";
-          src = ./.;
-          dontBuild = true;
-          doCheck = true;
-          nativeBuildInputs = [ pkgs.statix ];
-          checkPhase = ''
-            statix check .
-          '';
-          installPhase = "mkdir $out"; # Will fail otherwise.
-        };
-      });
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ nil ];
-      };
-
-      nixosModules = {
-        default = import ./modules/nixos/default.nix;
-      };
-
-      homeManagerModules = {
-        default = import ./modules/home-manager;
-      };
     };
 }
