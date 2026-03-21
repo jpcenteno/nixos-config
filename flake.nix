906a9b446aa80a582c54d05f581e643808facf0c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-03-21 11:16:10 -0300

    refactor(flake): Force output modularization
    This commit replaces the input module of `mkFlake` with an `import-tree`
    evaluation, removing the places where I might be tempted to add
    configuration at the top level. This is done in order to force
    modularization of the `output` attribute.

--- flake.nix
+++ flake.nix
@@ -15,11 +15,5 @@
     };
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        (inputs.import-tree ./modules)
-      ];
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
