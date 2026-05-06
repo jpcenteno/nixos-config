bc644e7a55df8f3825a4e40c7afcb916c2019dcc
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-05-06 11:03:48 -0300

    feat(wallpaper): Create $XDG_CONFIG_HOME/wallpapers dir on activation

--- modules/features/desktop/wallpaper.nix
+++ modules/features/desktop/wallpaper.nix
@@ -6,6 +6,9 @@
       pkgs,
       ...
     }:
+    let
+      wallpapersConfigHome = "${config.xdg.configHome}/wallpapers";
+    in
     {
       systemd.user.services.swaybg = {
         Unit = {
@@ -15,13 +18,19 @@
         };
 
         Service = {
-          ExecStart = "${lib.getExe pkgs.swaybg} -i ${config.xdg.configHome}/wallpapers/wallpaper";
+          ExecStart = "${lib.getExe pkgs.swaybg} -i ${wallpapersConfigHome}/wallpaper";
           Restart = "on-failure";
         };
 
         Install = {
           WantedBy = [ "graphical-session.target" ];
         };
+      };
+
+      home.activation = {
+        createWallpapersConfigHome = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          $DRY_RUN_CMD mkdir -p ${wallpapersConfigHome}
+        '';
       };
     };
 }
