0c6bcb80dcffb0707c06bfaa6e8a6525cd676992
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-05-08 15:08:23 -0300

    feat(wallpaper): Auto refresh wallpaper on change

--- modules/features/desktop/wallpaper.nix
+++ modules/features/desktop/wallpaper.nix
@@ -7,23 +7,53 @@
       ...
     }:
     let
+      target = "graphical-session.target";
       wallpapersConfigHome = "${config.xdg.configHome}/wallpaper";
+      currentWallpaperFilePath = "${wallpapersConfigHome}/current";
     in
     {
       systemd.user.services.swaybg = {
         Unit = {
           Description = "Set wallpaper with swaybg";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
+          PartOf = [ target ];
+          After = [ target ];
         };
 
         Service = {
-          ExecStart = "${lib.getExe pkgs.swaybg} -i ${wallpapersConfigHome}/current";
+          ExecStart = "${lib.getExe pkgs.swaybg} -i ${currentWallpaperFilePath}";
           Restart = "on-failure";
         };
 
         Install = {
-          WantedBy = [ "graphical-session.target" ];
+          WantedBy = [ target ];
+        };
+      };
+
+      systemd.user.paths.swaybg-wallpaper-watch = {
+        Unit = {
+          Description = "Watch wallpaper file for changes";
+          PartOf = [ target ]; # Ties lifecycle to `graphical-session`.
+          After = [ target ]; # Defers start until `graphical-session` is active.
+        };
+
+        Path = {
+          PathChanged = currentWallpaperFilePath; # Works both on file change and creation.
+          Unit = "refresh-wallpaper.service";
+        };
+
+        Install = {
+          WantedBy = [ target ];
+        };
+      };
+
+      systemd.user.services.refresh-wallpaper = {
+        Unit = {
+          Description = "Restart swaybg when wallpaper file changes";
+        };
+
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${lib.getExe' pkgs.systemd "systemctl"} --user restart swaybg.service";
         };
       };
 
