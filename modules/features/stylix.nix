3b348aaad3c4660c2d0852ba5cd981d041126a0c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-22 12:09:57 -0300

    stylix: Enable stylix

--- modules/features/stylix.nix
+++ modules/features/stylix.nix
@@ -4,12 +4,26 @@ let
 in
 {
   flake.modules = {
-    nixos.${name} = {
+    nixos.${name} = { pkgs, ... }: {
       imports = [ inputs.stylix.nixosModules.stylix ];
+      stylix = {
+        enable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+      };
     };
 
     homeManager.${name} = {
-      imports = [ inputs.stylix.homeModules.stylix ];
+      # TODO: Fix conflicts between Stylix and these modules, then remove the
+      # corresponding lines:
+      stylix.targets = {
+        alacritty.enable = false;
+        fzf.enable = false;
+        ghostty.enable = false;
+        hyprland.enable = false;
+        hyprlock.enable = false;
+        tmux.enable = false;
+        wofi.enable = false;
+      };
     };
   };
 }
