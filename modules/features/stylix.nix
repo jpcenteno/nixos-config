c6c6a8dad1ebd739c397d70af94814d92b3285c7
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-22 12:52:25 -0300

    stylix: Refactor module

--- modules/features/stylix.nix
+++ modules/features/stylix.nix
@@ -13,22 +13,24 @@ in
     };
 
     homeManager.${name} = { pkgs, ... }: {
-      stylix.cursor = {
-        package = pkgs.apple-cursor;
-        name = "macOS";
-        size = 30;
-      };
+      stylix = {
+        cursor = {
+          package = pkgs.apple-cursor;
+          name = "macOS";
+          size = 30;
+        };
 
-      # TODO: Fix conflicts between Stylix and these modules, then remove the
-      # corresponding lines:
-      stylix.targets = {
-        alacritty.enable = false;
-        fzf.enable = false;
-        ghostty.enable = false;
-        hyprland.enable = false;
-        hyprlock.enable = false;
-        tmux.enable = false;
-        wofi.enable = false;
+        # TODO: Fix conflicts between Stylix and these modules, then remove the
+        # corresponding lines:
+        targets = {
+          alacritty.enable = false;
+          fzf.enable = false;
+          ghostty.enable = false;
+          hyprland.enable = false;
+          hyprlock.enable = false;
+          tmux.enable = false;
+          wofi.enable = false;
+        };
       };
     };
   };
