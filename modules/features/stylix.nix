cd78fe593ddb699f104546302ce947e1813ae128
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-22 14:14:57 -0300

    stylix: Migrate font config to stylix module
    - Removes my original `fonts.nix` module.
    - Migrates the configuration to the `stylix` framework.

--- modules/features/stylix.nix
+++ modules/features/stylix.nix
@@ -20,6 +20,25 @@ in
           size = 30;
         };
 
+        fonts = {
+          emoji = {
+            name = "Noto Emoji";
+            package = pkgs.noto-fonts-monochrome-emoji;
+          };
+          monospace = {
+            name = "Lilex Nerd Font";
+            package = pkgs.nerd-fonts.lilex;
+          };
+          sansSerif = {
+            name = "IBM Plex Sans";
+            package = pkgs.ibm-plex;
+          };
+          serif = {
+            name = "IBM Plex Serif";
+            package = pkgs.ibm-plex;
+          };
+        };
+
         # TODO: Fix conflicts between Stylix and these modules, then remove the
         # corresponding lines:
         targets = {
