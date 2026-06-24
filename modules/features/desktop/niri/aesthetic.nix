0a10c68b79da9d782251f0f020e5309431eb374d
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-24 14:48:49 -0300

    Niri: Add corner radius

--- modules/features/desktop/niri/aesthetic.nix
+++ modules/features/desktop/niri/aesthetic.nix
@@ -1,8 +1,23 @@
+let
+  makeGeometryCornerRadius = radius: {
+    bottom-left = radius;
+    bottom-right = radius;
+    top-left = radius;
+    top-right = radius;
+  };
+in
 {
   flake.modules.homeManager.niri = {
     programs.niri.settings = {
       # Ask applications not to apply client-side decorations.
       prefer-no-csd = true;
+
+      window-rules = [
+        {
+          geometry-corner-radius = makeGeometryCornerRadius 8.0;
+          clip-to-geometry = true;
+        }
+      ];
     };
   };
 }
