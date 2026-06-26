0a4ecac1153472760174969ac61c1dbabd3bf4e1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-26 13:40:23 -0300

    bar/waybar(Refactor): Extract style module from main waybar module

--- modules/features/desktop/extras/bar/waybar/default.nix
+++ modules/features/desktop/extras/bar/waybar/default.nix
@@ -12,12 +12,6 @@
         systemd.enable = true;
         settings = {
           mainBar = {
-            position = "top";
-            # NOTE: Setting `programs.waybar.settings.<bar>.layer = "top"`
-            # anchors the bar to the display instead of each individual
-            # workspace. This means that it will stay fixed at it's `position`
-            # during the workspace change animation.
-            layer = "top";
             modules-left = [ "hyprland/workspaces" ];
             modules-right = builtins.concatLists [
               [
@@ -124,75 +118,6 @@
             };
           };
         };
-
-        # NOTE: Use the GUI debugger to To iterate faster on the CSS:
-        #
-        # 1. Stop the Waybar service: `systemctl stop --user waybar.service`.
-        # 2. Start the GUI debugger: `GTK_DEBUG=interactive waybar`.
-        #
-        # This will open a window with an object tree and a live CSS editor.
-        #
-        # NOTE: The Stylix module for Waybar provides definitions for the font
-        # size, family and all the @baseXX, colors regardles of the
-        # `stylix.targets.waybar.addCss` setting.
-        style = lib.mkAfter ''
-          * {
-            color: @base04; /* Alternate text */
-          }
-
-          button {
-            /*
-              Buttons are used as a label container on the workspaces module.
-              I chose to reset their spacing to simplify the stylesheet
-              selectors. Margins and paddings are all defined for the contained
-              labels instead.
-            */
-            padding: 0;
-            margin: 0;
-          }
-
-          label {
-            padding: 0.2rem 0.5rem;
-            margin-top: 0.4rem;
-            border-radius: 8px;
-            background-color: @base01; /* Alternate background. */
-          }
-
-          .focused label,
-          .active label {
-            background-color: @base0D; /* Focused window border color. */
-            color: @base01;
-          }
-
-          .urgent label {
-            /* Default text color because we may need more contrast. */
-            background-color: @base09; /* Urgent color */
-            color: @base01;
-          }
-
-          .modules-left button {
-            margin-right: 0.75rem;
-          }
-
-          .modules-right label {
-            margin-left: 0.75rem;
-          }
-
-          .modules-left {
-            margin-left: 0.75rem;
-          }
-
-          .modules-right {
-            margin-right: 0.75rem;
-          }
-        '';
       };
-
-      # Setting this to false to avoid fighting the default style sheet.
-      #
-      # NOTE Stylix will add some CSS regardless of this setting. I have left
-      # another note above the `programs.waybar.style` setting detailing on this
-      # matter.
-      stylix.targets.waybar.addCss = false;
     };
 }
