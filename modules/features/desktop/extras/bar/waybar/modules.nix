2d25a28ebcc8b01f86ec1390b6964790b950b118
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-06-26 13:52:21 -0300

    bar/waybar(Refactor): Extract Waybar modules to their own .nix file

---
+++ modules/features/desktop/extras/bar/waybar/modules.nix
@@ -0,0 +1,117 @@
+{
+  flake.modules.homeManager.bar =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      programs.waybar.settings.mainBar = {
+        modules-left = [ "hyprland/workspaces" ];
+        modules-right = builtins.concatLists [
+          [
+            "cpu"
+            "memory"
+            "temperature"
+            "network"
+            "disk"
+            "bluetooth"
+            "pulseaudio"
+            "battery"
+            "custom/clock"
+          ]
+        ];
+        "custom/clock" = {
+          # Reinventing the wheel here to avoid desynchronization issues I was
+          # having with `timedatectl`.
+          exec = "date '+%H:%M'";
+          format = " {}";
+          interval = 10;
+        };
+        battery = {
+          interval = 15;
+          states = {
+            "good" = 95;
+            "warning" = 30;
+            "critical" = 15;
+          };
+          format = "{icon}  {capacity}%";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          format-charging = "󰂄 {capacity}%";
+        };
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-bluetooth = "{icon}  {volume}%";
+          format-muted = "󰝟";
+          format-icons = {
+            default = [
+              "󰕿"
+              "󰖀"
+              "󰕾"
+            ];
+          };
+        };
+        cpu = {
+          format = "  {}%";
+        };
+        temperature = {
+          format = "{icon} {temperatureC}°C";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+        network = {
+          interval = 10;
+          format-wifi = " ";
+          format-ethernet = "󰈀";
+          tooltip-format-wifi = ''
+             {essid}
+            󱑻 {frequency} GHz
+            󱄙 {signaldBm} ({signalStrength}%)
+
+             {ifname}
+
+            ↓ {bandwidthDownBytes}
+            ↑ {bandwidthUpBytes}'';
+          tooltip-format-ethernet = ''
+             {ifname}
+
+            ↓ {bandwidthDownBytes}
+            ↑ {bandwidthUpBytes}'';
+        };
+        memory = {
+          format = "  {}%";
+        };
+        disk = {
+          format = "  {percentage_used}%";
+        };
+        bluetooth = {
+          format-connected = " {num_connections}";
+          on-click = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.bluetuith}/bin/bluetuith";
+          tooltip-format = ''
+            {controller_alias}	{controller_address}
+
+            {num_connections} connected'';
+          tooltip-format-connected = ''
+            {controller_alias}	{controller_address}
+
+            {num_connections} connected
+
+            {device_enumerate}'';
+          tooltip-format-enumerate-connected = "{device_alias}	{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}	{device_address}	{device_battery_percentage}%";
+        };
+      };
+    };
+}
