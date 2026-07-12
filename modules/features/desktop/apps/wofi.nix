fcc248d16f9afad6a8699bab693886f529dd28a7
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-07-12 12:13:27 -0300

    Wofi: Create Wofi module. Customize.
    - Creates `wofi` module for home manager.
    - Enables Stylix module for Wofi.
    - Enables case-insensitive matching.
    - Enables images.

---
+++ modules/features/desktop/apps/wofi.nix
@@ -0,0 +1,12 @@
+{
+  flake.modules.homeManager.wofi = {
+    programs.wofi = {
+      enable = true;
+      settings = {
+        allow_images = true;
+        image_size = 24;
+        insensitive = true;
+      };
+    };
+  };
+}
